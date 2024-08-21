398a6bebc95a84793d1097504de9be2ecc2a7bbb
Valdinei Ferreira <valdineifer.dev@outlook.com>
2024-08-21 00:23:56 -0300

    chore: add production files
    - reset migrations
    - add Dockerfile (for Node setup) and Procfile (for migrations)
    - adjust schema to add timezone and recommended ids

--- app/database/schema.ts
+++ app/database/schema.ts
@@ -1,6 +1,6 @@
 import { relations } from 'drizzle-orm';
 import {
-  integer, json, jsonb, pgTable, serial, text, timestamp,
+  integer, jsonb, pgTable, text, timestamp,
   uuid,
 } from 'drizzle-orm/pg-core';
 import { ComputerInfo, Settings } from '~/types/models';
@@ -14,29 +14,29 @@ export const laboratory = pgTable('laboratory', {
 });
 
 export const computer = pgTable('computer', {
-  id: serial('id').primaryKey(),
+  id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
   mac: text('mac').notNull().unique(),
   name: text('name').notNull().unique(),
   laboratoryId: integer('laboratory_id').references(() => laboratory.id),
-  info: json('info').$type<ComputerInfo>(),
+  info: jsonb('info').$type<ComputerInfo>(),
   token: uuid('token').notNull().unique().defaultRandom(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
 });
 
 export const computerLog = pgTable('computer_log', {
-  id: serial('id').primaryKey(),
+  id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
   computerId: integer('computer_id').references(() => computer.id),
-  oldObject: json('old_object').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
+  oldObject: jsonb('old_object').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
 });
 
 export const user = pgTable('user', {
-  id: serial('id').primaryKey(),
+  id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
   username: text('username').notNull().unique(),
   password: text('password').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
 });
 
 export const settings = pgTable('settings', {
