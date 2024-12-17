c20e3dc2d016a447a94019389538042066396027
Valdinei Ferreira <valdineifer.dev@outlook.com>
2024-12-16 22:17:42 -0300

    fix(computers): adjust links and remove part of text
    - include the numbers in cards as a link
    - remove text about the default minimum storage

--- app/routes/_app+/_index.tsx
+++ app/routes/_app+/_index.tsx
@@ -22,17 +22,17 @@ export default function Index() {
   return (
     <main className="flex flex-1 flex-row gap-4">
       <Card className='sm:w-1/2 md:w-1/3 xl:w-1/4'>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            <Link className='hover:underline hover:underline-offset-4' to='/computers'>
+        <Link className='hover:underline hover:underline-offset-4' to='/computers'>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
                 Computadores
-            </Link>
-          </CardTitle>
-          <Computer className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">{count.total}</div>
-        </CardContent>
+            </CardTitle>
+            <Computer className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{count.total}</div>
+          </CardContent>
+        </Link>
         <CardFooter>
           <div className="text-xs text-muted-foreground">
               Contagens apenas consideram os computadores não-rejeitados.
@@ -40,36 +40,36 @@ export default function Index() {
         </CardFooter>
       </Card>
       <Card className='sm:w-1/2 md:w-1/3 xl:w-1/4'>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            <Link className='hover:underline hover:underline-offset-4' to="/computers?inactive">
+        <Link className='hover:underline hover:underline-offset-4' to="/computers?inactive">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
                 Computadores inativos
-            </Link>
-          </CardTitle>
-          <PowerOff className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">{count.inactive}</div>
-        </CardContent>
+            </CardTitle>
+            <PowerOff className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{count.inactive}</div>
+          </CardContent>
+        </Link>
         <CardFooter>
           <div className="text-xs text-muted-foreground">Sem atualizações há mais de 7 dias</div>
         </CardFooter>
       </Card>
       <Card className='sm:w-1/2 md:w-1/3 xl:w-1/4'>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            <Link className='hover:underline hover:underline-offset-4' to="/computers?lowStorage">
+        <Link className='hover:underline hover:underline-offset-4' to="/computers?lowStorage">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
                 Com pouco espaço em disco
-            </Link>
-          </CardTitle>
-          <HardDrive className="size-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">{count.lowStorage}</div>
-        </CardContent>
+            </CardTitle>
+            <HardDrive className="size-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{count.lowStorage}</div>
+          </CardContent>
+        </Link>
         <CardFooter>
           <div className="text-xs text-muted-foreground">
-              Com base na configuração de espaço mínimo. (Padrão: 20GB)
+              Com base na configuração de espaço mínimo.
           </div>
         </CardFooter>
       </Card>
